0b3c9f9f9451a2b98a5641627804842c088a5b44
Foat Akhmadeev <foat@fmga.ru>
2025-12-14 10:25:37 +0300

    bump versions; autoconf node 20

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from hub_full_paths import find_and_fix_dockerfiles
 from replace_in_dir import replace_in_directory
 
 ########################################## Downloading containers folders...
-download_github_tree("https://github.com/linuxserver/docker-baseimage-alpine/tree/3.22")
+download_github_tree("https://github.com/linuxserver/docker-baseimage-alpine/tree/3.23")
 download_github_tree("https://github.com/bitnami/containers/tree/main/bitnami/openldap")
 # original: "https://github.com/MatthewVance/unbound-docker/tree/master/1.22.0"
 download_github_tree(
